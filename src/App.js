fdbb3b498b8be942e2bbed05a4365d8ccf7843e2
90322 <90322@sistemas.frc.utn.edu.ar>
2023-06-13 00:31:31 -0300

    autos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Inicio from './components/Inicio.jsx';
+import Menu from './components/menu.jsx';
+import Autos from './components/autos/Autos.jsx';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Menu />
+        <div className="divBody">
+          <Routes>
+            <Route path="/inicio" element={<Inicio />} />
+            <Route path="/autos" element={<Autos />} />
+            <Route path="*" element={<Navigate to="/Inicio" replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Inicio.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Inicio() {
+  return (
+    <div className="mt-4 p-5 rounded" style={{ backgroundColor: 'lightgray' }}>
+      <h1>TPI 2023</h1>
+      <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
+      <p>
+        Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y
+        múltiples capas en Javascript.
+      </p>
+      <p>
+        Frontend: Single Page Aplication, HTML, CSS, Bootstrap, NodeJs,
+        Javascript y React.
+      </p>
+    </div>
+  );
+}
