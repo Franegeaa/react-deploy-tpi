67c6007a6826d46e65967d1bbb3df9c495e7f4d5
alejo herrera <85969@sistemas.frc.utn.edu.ar>
2023-06-20 15:45:39 -0300

    edit componente inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,16 +3,13 @@ import React from 'react';
 export default function Inicio() {
   return (
     <div className="mt-4 p-5 rounded" style={{ backgroundColor: 'lightgray' }}>
+      <title>Integrantes del Grupo</title>
       <h1>TPI 2023</h1>
-      <p>Este ejemplo está desarrollado con las siguientes tecnologías:</p>
-      <p>
-        Backend: NodeJs, Express , WebApiRest, Swagger, Sequelize, Sqlite y
-        múltiples capas en Javascript.
-      </p>
-      <p>
-        Frontend: Single Page Aplication, HTML, CSS, Bootstrap, NodeJs,
-        Javascript y React.
-      </p>
+      <h1>Integrantes del Grupo</h1>
+        <h2>Anael Gonzalez</h2>
+        <h2>Alejo Herrera</h2>
+        <h2>Francisco Egea</h2>
+        <h2>Abel Castillo</h2>
     </div>
   );
 }
